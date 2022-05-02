dbabb489a7778b27c0de1ab89b2bf68547f5f472
Tassol <carol.o.borin@gmail.com>
2022-05-02 23:23:11 +0200

    First commmit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseHttpsRedirection();
+
+var movies = new List<Movie>();
+movies.Add(new Movie { Id = 1, Name = "The Godfather", Genre = "Crime", Rating = 90 });
+movies.Add(new Movie { Id = 2, Name = "Titanic", Genre = "Romance", Rating = 85 });
+movies.Add(new Movie { Id = 3, Name = "The Exorcist", Genre = "Horror", Rating = 80 });
+
+
+app.MapGet("/getallmovies", () =>
+{
+    return movies;
+});
+
+app.MapPost("", (Movie movie) =>
+{
+    movies.Add(movie);
+    return movies;
+});
+
+app.MapPut("", (Movie movie) =>
+{
+    var index = movies.FindIndex(m => m.Id == movie.Id);
+    movies[index] = movie;
+    return movies;
+});
+
+app.MapDelete("/{id}", (int id) =>
+{
+    var index = movies.FindIndex(m => m.Id == id);
+    movies[index] = null;
+    return movies;
+});
+
+app.UseSwaggerUI();
+app.Run();
+
+public class Movie
+{
+    public int Id
+    {
+        get;
+        set;
+    }
+    public string? Name
+    {
+        get;
+        set;
+    }
+    public string? Genre
+    {
+        get;
+        set;
+    }
+    public decimal Rating
+    {
+        get;
+        set;
+    }
+}
